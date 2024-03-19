6d8fd0271d93d65e45e7175a22964ae42eb847cf
איתן קטייב <etanss9911@gmail.com>
2024-03-19 13:49:56 +0200

    (CustomWindowUI.cpp) Changed colors to defines

--- Pixel_Letter_Station/CustomWindowUI.cpp
+++ Pixel_Letter_Station/CustomWindowUI.cpp
@@ -24,9 +24,9 @@ bool QuitButton(LPARAM lParam, int buttonId) // Bar Quit Button
 
 		// Shine
 		SetRect(&effectRect, pDIS->rcItem.left, pDIS->rcItem.top, pDIS->rcItem.right, pDIS->rcItem.top + BORDER_EFFECT_SIZE); 
-		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(RGB(255, 100, 100))); 
+		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(RGB(255, 100, 100)));
 		SetRect(&effectRect, pDIS->rcItem.left, pDIS->rcItem.top, pDIS->rcItem.left + BORDER_EFFECT_SIZE, pDIS->rcItem.bottom); 
-		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(RGB(255, 100, 100))); 
+		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(RGB(255, 100, 100)));
 
 		// Prepare the rectangle for the text, adjusting if the button is pressed
 		RECT textRect = pDIS->rcItem;
@@ -111,31 +111,31 @@ bool DefaultButton(LPARAM lParam, const wchar_t* Text, int buttonId) // GUI Defa
 		// Set the background and text colors
 		SetTextColor(pDIS->hDC, RGB(0, 0, 0));
 		SetBkMode(pDIS->hDC, TRANSPARENT);
-		FillRect(pDIS->hDC, &pDIS->rcItem, CreateSolidBrush(RGB(255, 255, 255)));
+		FillRect(pDIS->hDC, &pDIS->rcItem, CreateSolidBrush(DEFULT_BUTTON_COLOR));
 
 		// Button shading
 		RECT effectRect;
 
 		// Shadow
 		SetRect(&effectRect, pDIS->rcItem.left, pDIS->rcItem.bottom - BORDER_EFFECT_SIZE, pDIS->rcItem.right, pDIS->rcItem.bottom);
-		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(RGB(160, 120, 95)));
+		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(DEFULT_BUTTON_COLOR_SHADOW));
 		SetRect(&effectRect, pDIS->rcItem.right - BORDER_EFFECT_SIZE, pDIS->rcItem.top, pDIS->rcItem.right, pDIS->rcItem.bottom);
-		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(RGB(160, 120, 95)));
+		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(DEFULT_BUTTON_COLOR_SHADOW));
 
 		// Shine
 		SetRect(&effectRect, pDIS->rcItem.left, pDIS->rcItem.top, pDIS->rcItem.right, pDIS->rcItem.top + BORDER_EFFECT_SIZE);
-		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(RGB(255, 240, 200)));
+		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(DEFULT_BUTTON_COLOR_SHINE));
 		SetRect(&effectRect, pDIS->rcItem.left, pDIS->rcItem.top, pDIS->rcItem.left + BORDER_EFFECT_SIZE, pDIS->rcItem.bottom);
-		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(RGB(255, 240, 200)));
+		FillRect(pDIS->hDC, &effectRect, CreateSolidBrush(DEFULT_BUTTON_COLOR_SHINE));
 
 		// Prepare the rectangle for the text, adjusting if the button is pressed
 		RECT textRect = pDIS->rcItem;
 		if (isPressed) {
 			// Offset the textRect and change color to simulate the text moving when pressed
 			OffsetRect(&textRect, 1, 1);
-			SetTextColor(pDIS->hDC, RGB(255, 255, 255));
-			SetBkColor(pDIS->hDC, RGB(200, 200, 255));
-			FillRect(pDIS->hDC, &pDIS->rcItem, CreateSolidBrush(RGB(200, 200, 255)));
+			SetTextColor(pDIS->hDC, DEFULT_BUTTON_COLOR_PRESSED_TEXT);
+			SetBkColor(pDIS->hDC, DEFULT_BUTTON_COLOR_PRESSED);
+			FillRect(pDIS->hDC, &pDIS->rcItem, CreateSolidBrush(DEFULT_BUTTON_COLOR_PRESSED));
 		}
 
 		// Draw the button text using the adjusted rectangle
@@ -191,7 +191,7 @@ bool Title(HDC hdc, HWND hWnd, int centerW) // The title of the application in t
 void WindowBar(HDC hdc, HWND hWnd, int width)
 {
 	// Set the color for the rectangle (optional)
-	HBRUSH brush = CreateSolidBrush(RGB(255, 100, 100)); // orangeish color
+	HBRUSH brush = CreateSolidBrush(WINODW_UI_COLOR); // orangeish color
 	HPEN nullPen = CreatePen(PS_NULL, 1, RGB(0, 0, 0)); // Null pen, color doesn't matter
 	SelectObject(hdc, brush);
 	SelectObject(hdc, nullPen);
@@ -201,13 +201,13 @@ void WindowBar(HDC hdc, HWND hWnd, int width)
 	Rectangle(hdc, 0, 0, width, WIN_BAR_SIZE);
 
 	// Draw the Border Shadow
-	brush = CreateSolidBrush(RGB(155, 0, 0)); // orangeish color
+	brush = CreateSolidBrush(WINODW_UI_COLOR_SHADOW); // orangeish color
 	SelectObject(hdc, brush);
 	Rectangle(hdc, 0, WIN_BAR_SIZE, width, WIN_BAR_SIZE - BORDER_EFFECT_SIZE);
 	Rectangle(hdc, width - BORDER_EFFECT_SIZE + 1, 0, width + 1, WIN_BAR_SIZE);
 
 	// Draw the Border shine
-	brush = CreateSolidBrush(RGB(255, 200, 200)); // orangeish color
+	brush = CreateSolidBrush(WINDOW_UI_COLOR_SHINE); // orangeish color
 	SelectObject(hdc, brush);
 	Rectangle(hdc, 0, 0, width + 1, BORDER_EFFECT_SIZE);
 	Rectangle(hdc, 0, 0, BORDER_EFFECT_SIZE, WIN_BAR_SIZE);
@@ -222,7 +222,7 @@ void WindowBar(HDC hdc, HWND hWnd, int width)
 void WindowFrame(HDC hdc, HWND hWnd, int width, int height)
 {
 	// Create a pen of desired thickness and color
-	HPEN hPen = CreatePen(PS_SOLID, BAR_MARGIN * 2, RGB(255, 100, 100)); // Black pen
+	HPEN hPen = CreatePen(PS_SOLID, BAR_MARGIN * 2, WINODW_UI_COLOR); // Black pen
 
 	// Select the pen and a null brush into the DC
 	HPEN hOldPen = (HPEN)SelectObject(hdc, hPen);
@@ -242,17 +242,17 @@ void WindowFrame(HDC hdc, HWND hWnd, int width, int height)
 void LetterBackground(HDC hdc, HWND hWnd, int width, int height)
 {
 	// First, draw the larger rectangle with a solid color
-	HBRUSH brushMain = CreateSolidBrush(RGB(50, 0, 0)); // Black color for the Border
+	HBRUSH brushMain = CreateSolidBrush(LETTER_BORDER); // Black color for the Border
 	RECT rectMain = { width - LETTER_BOX_WIDTH - MARGIN - SMALL_MARGIN, MARGIN * 2.5, width - (SMALL_MARGIN * 2), height - (MARGIN * 3)}; // Main rectangle coordinates
 	FillRect(hdc, &rectMain, brushMain);
 
 	//Then, the "Paper" by drawing it with the paper color
-	HBRUSH brushShading = CreateSolidBrush(RGB(200, 160, 50)); // Brush for the Shadow, using the shadow color
+	HBRUSH brushShading = CreateSolidBrush(PAPER_COLOR_SHADOW); // Brush for the Shadow, using the shadow color
 	RECT rectShading = { (width - (LETTER_BOX_WIDTH / 2) - MARGIN + BORDER_EFFECT_SIZE) - (LETTER_BOX_WIDTH / 2) - SMALL_MARGIN, (MARGIN * 2.5) + BORDER_EFFECT_SIZE, width - (SMALL_MARGIN * 2) - BORDER_EFFECT_SIZE, height - (MARGIN * 3) - BORDER_EFFECT_SIZE }; // Smaller rectangle coordinates for the cutout
 	FillRect(hdc, &rectShading, brushShading);
 
 	//Then, the "Paper" by drawing it with the paper color
-	HBRUSH brushPaper = CreateSolidBrush(RGB(255, 223, 133)); // Brush for the cutout, using the paper background color
+	HBRUSH brushPaper = CreateSolidBrush(PAPER_COLOR); // Brush for the cutout, using the paper background color
 	RECT rectCutout = { (width - (LETTER_BOX_WIDTH / 2) - MARGIN + (BORDER_EFFECT_SIZE * 2)) - (LETTER_BOX_WIDTH / 2) - SMALL_MARGIN, (MARGIN * 2.5) + (BORDER_EFFECT_SIZE * 2), width - (SMALL_MARGIN * 2) - BORDER_EFFECT_SIZE, height - (MARGIN * 3) - BORDER_EFFECT_SIZE }; // Smaller rectangle coordinates for the cutout
 	FillRect(hdc, &rectCutout, brushPaper);
 
@@ -271,7 +271,7 @@ void RichTextBoxPaint(HWND box)
 	pf.bLineSpacingRule = 5;
 	pf.dyLineSpacing = 30;
 
-	SendMessage(box, EM_SETBKGNDCOLOR, 0, (LPARAM)RGB(255, 223, 133)); // BKG color
+	SendMessage(box, EM_SETBKGNDCOLOR, 0, (LPARAM)PAPER_COLOR); // BKG color
 	SendMessage(box, EM_SETPARAFORMAT, 0, (LPARAM)&pf); // Sends format
 	SendMessage(box, EM_SETLIMITTEXT, (WPARAM)1400, 0); // Sends Text char limit
 
@@ -280,17 +280,17 @@ void RichTextBoxPaint(HWND box)
 void ServerStatusBar(HDC hdc, int isConnected)
 {
 	// First, draw the larger rectangle with a solid color
-	HBRUSH brushMain = CreateSolidBrush(RGB(50, 0, 0)); // Black color for the Border
+	HBRUSH brushMain = CreateSolidBrush(UI_BORDER); // Black color for the Border
 	RECT rectMain = { MARGIN * 1.5, MARGIN * 2.5, MARGIN * 10, (MARGIN * 4) + SMALL_MARGIN }; // Main rectangle coordinates
 	FillRect(hdc, &rectMain, brushMain);
 
 	//Then, the "Shine" by drawing it with the Shine color
-	HBRUSH brushShading = CreateSolidBrush(RGB(150, 100, 70)); // Brush for the cutout, using the Shine color
+	HBRUSH brushShading = CreateSolidBrush(UI_BORDER_SHINE); // Brush for the cutout, using the Shine color
 	RECT rectShading = { MARGIN * 1.5 + BORDER_EFFECT_SIZE, MARGIN * 2.5 + BORDER_EFFECT_SIZE, MARGIN * 10 - BORDER_EFFECT_SIZE, (MARGIN * 4) + SMALL_MARGIN - BORDER_EFFECT_SIZE }; // Smaller rectangle coordinates for the cutout
 	FillRect(hdc, &rectShading, brushShading);
 
 	//Then, the "Paper" by drawing it with the paper color
-	HBRUSH brushPaper = CreateSolidBrush(RGB(100, 50, 50)); // Brush for the cutout, using the window background color
+	HBRUSH brushPaper = CreateSolidBrush(UI_BORDER_SHADOW); // Brush for the cutout, using the window background color
 	RECT rectCutout = { MARGIN * 1.5 + (BORDER_EFFECT_SIZE * 2), MARGIN * 2.5 + (BORDER_EFFECT_SIZE * 2), MARGIN * 10 - BORDER_EFFECT_SIZE, (MARGIN * 4) + SMALL_MARGIN - BORDER_EFFECT_SIZE }; // Smaller rectangle coordinates for the cutout
 	FillRect(hdc, &rectCutout, brushPaper);
 
@@ -318,17 +318,17 @@ void ServerStatusBar(HDC hdc, int isConnected)
 	TextOut(hdc, MARGIN * 2, MARGIN * 3, L"Status:", strlen("Status:"));
 	if (isConnected == 0)
 	{
-		SetTextColor(hdc, RGB(255, 100, 100));// text color
+		SetTextColor(hdc, STATUS_OFFLINE_COLOR);// text color
 		TextOut(hdc, MARGIN * 5, MARGIN * 3, L"Offline", strlen("Offline"));
 	}
 	if (isConnected == 1)
 	{
-		SetTextColor(hdc, RGB(100, 255, 100));// text color
+		SetTextColor(hdc, STATUS_ONLINE_COLOR);// text color
 		TextOut(hdc, MARGIN * 5, MARGIN * 3, L"Connected", strlen("Connected"));
 	}
 	if (isConnected == 2)
 	{
-		SetTextColor(hdc, RGB(150, 150, 255));// text color
+		SetTextColor(hdc, STATUS_Server_COLOR);// text color
 		TextOut(hdc, MARGIN * 5, MARGIN * 3, L"server", strlen("server"));
 	}
 
