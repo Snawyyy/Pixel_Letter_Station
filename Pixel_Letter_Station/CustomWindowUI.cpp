ab5fbed935711eee68501c5f33b04e8fb2b5f674
איתן קטייב <etanss9911@gmail.com>
2024-04-17 03:58:40 +0300

    edited letter background to use defines for easier future changes

--- Pixel_Letter_Station/CustomWindowUI.cpp
+++ Pixel_Letter_Station/CustomWindowUI.cpp
@@ -404,17 +404,38 @@ void LetterBackground(HDC hdc, HWND hWnd, int width, int height)
 {
 	// First, draw the border, a larger rectangle with a solid color
 	HBRUSH brushMain = CreateSolidBrush(LETTER_BORDER); // Black color for the Border
-	RECT rectMain = { width - LETTER_BOX_WIDTH - MARGIN - SMALL_MARGIN, MARGIN * 2.5, width - (SMALL_MARGIN * 2), height - (MARGIN * 3)}; // Main rectangle coordinates
+
+	// Main rectangle rect
+	RECT rectMain = { 
+		LETTER_BOX_RECT_LEFT, // left
+		LETTER_BOX_RECT_TOP, // top
+		LETTER_BOX_RECT_RIGHT, // right
+		LETTER_BOX_RECT_BOTTOM};  // bottom
+
 	FillRect(hdc, &rectMain, brushMain);
 
 	//Then, the "Shadow" by drawing it with the paper color
 	HBRUSH brushShading = CreateSolidBrush(PAPER_COLOR_SHADOW); // Brush for the Shadow, using the shadow color
-	RECT rectShading = { (width - (LETTER_BOX_WIDTH / 2) - MARGIN + BORDER_EFFECT_SIZE) - (LETTER_BOX_WIDTH / 2) - SMALL_MARGIN, (MARGIN * 2.5) + BORDER_EFFECT_SIZE, width - (SMALL_MARGIN * 2) - BORDER_EFFECT_SIZE, height - (MARGIN * 3) - BORDER_EFFECT_SIZE }; // Smaller rectangle coordinates for the cutout
+
+	// Smaller rectangle coordinates for the cutout
+	RECT rectShading = {
+		LETTER_BOX_RECT_LEFT + BORDER_EFFECT_SIZE, // left
+		LETTER_BOX_RECT_TOP + BORDER_EFFECT_SIZE, // top
+		LETTER_BOX_RECT_RIGHT - BORDER_EFFECT_SIZE, // right
+		LETTER_BOX_RECT_BOTTOM - BORDER_EFFECT_SIZE }; // bottom
+
 	FillRect(hdc, &rectShading, brushShading);
 
 	//Then, the "Paper" by drawing it with the paper color
 	HBRUSH brushPaper = CreateSolidBrush(PAPER_COLOR); // Brush for the cutout, using the paper background color
-	RECT rectCutout = { (width - (LETTER_BOX_WIDTH / 2) - MARGIN + (BORDER_EFFECT_SIZE * 2)) - (LETTER_BOX_WIDTH / 2) - SMALL_MARGIN, (MARGIN * 2.5) + (BORDER_EFFECT_SIZE * 2), width - (SMALL_MARGIN * 2) - BORDER_EFFECT_SIZE, height - (MARGIN * 3) - BORDER_EFFECT_SIZE }; // Smaller rectangle coordinates for the cutout
+
+	// Smaller rectangle coordinates for the cutout
+	RECT rectCutout = {
+		LETTER_BOX_RECT_LEFT + BORDER_EFFECT_SIZE * 2, // left
+		LETTER_BOX_RECT_TOP + BORDER_EFFECT_SIZE * 2, // top
+		LETTER_BOX_RECT_RIGHT - BORDER_EFFECT_SIZE, // right
+		LETTER_BOX_RECT_BOTTOM - BORDER_EFFECT_SIZE }; // bottom
+
 	FillRect(hdc, &rectCutout, brushPaper);
 
 	// Clean up
